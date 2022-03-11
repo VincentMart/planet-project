e020a527efe8dfb57f88e48a7b7420f985bcd6bd
vimartin12 <vincent.martin2@etu.uca.fr>
2022-03-11 14:55:08 +0100

    commit base en mieux

--- 02_HelloWorldAdvanced/Base.cs
+++ 02_HelloWorldAdvanced/Base.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using Windows.ApplicationModel.Core;
+using Urho;
+using Urho.Actions;
+using Urho.SharpReality;
+using Urho.Shapes;
+using Urho.Resources;
+using System;
 
-namespace _07_HelloWorldWithCustomShaders
+namespace SolarSystem
 {
     internal class Base
     {
+        Node parentNode;
+        Material planetMaterial;
+
+        public Base(Node planetNode)
+        {
+            this.parentNode = parentNode;
+            planetMaterial = RessourceCache.GetMaterial("Materials/Sun.xml");
+
+            var basePlanet = parentNode.CreateChild();
+            basePlanet.SetScale(0.99f);
+            basePlanet.Position = new Vector3(0, 0, 0);
+            var baseP = basePlanet.CreateComponent<Sphere>();
+            baseP.SetMaterial(planetMaterial);
+        } 
     }
 }
--- 02_HelloWorldAdvanced/Program.cs
+++ 02_HelloWorldAdvanced/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Windows.ApplicationModel.Core;
 using Urho;
 using Urho.Actions;
